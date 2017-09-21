c88a96838c4ac564957dece22e8ca4cbff870209
shailendra <shailendra@healint.com>
2017-09-21 12:19:46 +0800

    CurrencyServiceImplementation class mock created

---
+++ CurrencyConversion/app/src/main/java/services/CurrencyServiceImpl.java
@@ -0,0 +1,24 @@
+package services;
+
+import java.util.List;
+
+import modals.Currency;
+import modals.Exchange;
+
+/**
+ * Created by SKY on 9/21/2017.
+ */
+
+public class CurrencyServiceImpl implements CurrencyService {
+
+	public List<Currency> getAllCurrencies()
+	{
+		return null;
+	}
+
+	public Exchange getExchangeRate(Currency base, Currency target)
+	{
+		return null;
+	}
+
+}
