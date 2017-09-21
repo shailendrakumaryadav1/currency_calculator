d43dc7a29e399eaeb3684c6da2391166e19387b5
shailendra <shailendra@healint.com>
2017-09-21 12:12:07 +0800

    services package added. CurrencyService interface created

---
+++ CurrencyConversion/app/src/main/java/services/CurrencyService.java
@@ -0,0 +1,15 @@
+package services;
+
+import java.util.List;
+
+import modals.Currency;
+
+/**
+ * Created by SKY on 9/21/2017.
+ */
+
+public interface CurrencyService {
+
+	public List<Currency> getAllCurrencies();
+
+}
